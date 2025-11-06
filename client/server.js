786c4de16d03b4f7ed22c0c111c7a6f67c345c42
jimmy1720 <90459798+jimmy1720@users.noreply.github.com>
2025-11-06 13:26:30 +0800

    change to use static virtual directory

--- client/server.js
+++ client/server.js
@@ -3,9 +3,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/css/:file", (req,res) => {
-	res.sendFile(__dirname + '/views/css/' + req.params.file);
-});
+app.use(express.static('views'));
+app.use("/css", express.static(__dirname + '/views/css'));
 
 app.get("/", (req,res) => {
 	res.status(200).render('index',{title:"Home"});	
@@ -19,4 +18,7 @@ app.get("/register", (req,res) => {
 	res.status(200).render('register',{title:"Register"});	
 });
 
-app.listen(process.env.PORT || 8099);
+const server = app.listen(process.env.PORT || 8099, () => { 
+    const port = server.address().port;
+    console.log(`Server is listening at port ${port}`); 
+});
